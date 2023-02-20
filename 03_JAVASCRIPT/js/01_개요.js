7bcdfc60c72c9ca35bc0649524b84fdcdca34c81
MINSU KIM <mskim9097@gmail.com>
2023-02-20 15:13:28 +0900

    20230220 commit

---
+++ 03_JAVASCRIPT/js/01_개요.js
@@ -0,0 +1,17 @@
+// 한 줄 주석
+/* 범위 주석 */
+
+// js파일은 <script> 태그 내부라고 생각하면 됨
+
+
+function btnClick2() {
+    alert("external 버튼이 클릭됨.");
+}
+
+function changeColor1() {
+    document.getElementById("box").style.backgroundColor = "red";
+}
+
+function changeColor2() {
+    document.getElementById("box").style.backgroundColor = "yellow";
+}
