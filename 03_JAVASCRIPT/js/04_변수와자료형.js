f6ac24cb862eebd9d0cc99794d3d61ad69cfa638
mskim9097@gmail.com <mskim9097@gmail.com>
2023-02-23 10:49:14 +0900

    20230223 commit

---
+++ 03_JAVASCRIPT/js/04_변수와자료형.js
@@ -0,0 +1,11 @@
+var str = "JS 코드를 함수가 아닌"
+        + "JS 파일 또는 script태그 내부에 바로 작성하면"
+        + "HTML 랜더링 시 바로 수행된다.";
+
+console.log(str);
+
+var num1 = 100; // 전역변수
+num2 = 200; // 전역변수
+
+console.log("num1 : " + num1 )
+console.log("num2 : " + num2 )
